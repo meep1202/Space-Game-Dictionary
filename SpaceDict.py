a06e896db0f25f22cbc7283aa6ac8b3480e3db04
meep1202 <168873599+meep1202@users.noreply.github.com>
2024-05-04 19:37:41 -0700

    Create SpaceDict.py

---
+++ SpaceDict.py
@@ -0,0 +1,454 @@
+classes = {
+    "1": {
+        "Name": "Drone Operator",
+        "Weapon": {
+            "1": {
+                "Weapon Name": "Old Carbine",
+                "Weapon Damage": 10,
+                "Weapon Accuracy": 85,
+                "Weapon Crit Chance": 10
+            },
+            "2": {
+                "Weapon Name": "Laser Carbine",
+                "Weapon Damage": 15,
+                "Weapon Accuracy": 87,
+                "Weapon Crit Chance": 12
+            }
+        },
+        "Armor": {
+            "1": {
+                "Armor Name": "Infantry Armor",
+                "Armor Defense": 7,
+                "Armor Dodge Chance": 0
+            },
+            "2": {
+                "Armor Name": "Heavy Plate",
+                "Armor Defense": 10,
+                "Armor Dodge Chance": 0
+            },
+            "3": {
+                "Armor Name": "Exoskeleton",
+                "Armor Defense": 15,
+                "Armor Dodge Chance": 0
+            }
+        },
+        "Class Abilities": {
+            "1": "Assisted Fire",
+            "2": "Energy Shield",
+            "Passive 1": "Drone",
+        },
+        "Subclass": {
+            "1": {
+                "Subclass Name": "Medic",
+                "Subclass Abilities": {
+                    "3": "Heal",
+                    "4": "Stim",
+                    "5": "Stasis",
+                    "6": "Restoration",
+                    "Passive 2": "Triage"
+                },
+                "Subclass Weapon": {
+                    "3": {
+                        "Weapon Name": "Tesla Carbine",
+                        "Weapon Damage": 20,
+                        "Weapon Accuracy": 90,
+                        "Weapon Crit Chance": 15
+                    },
+                    "4": {
+                        "Weapon Name": "Disintegrator",
+                        "Weapon Damage": 25,
+                        "Weapon Accuracy": 95,
+                        "Weapon Crit Chance": 15
+                    }
+                }
+            },
+            "2": {
+                "Subclass Name": "Bombardier",
+                "Subclass Abilities": {
+                    "3": "Micro Missile",
+                    "4": "Smokescreen",
+                    "5": "Napalm",
+                    "6": "Rocket Barrage",
+                    "Passive 2": "ASI"
+                },
+                "Subclass Weapon": {
+                    "3": {
+                        "Weapon Name": "Tesla Carbine",
+                        "Weapon Damage": 20,
+                        "Weapon Accuracy": 90,
+                        "Weapon Crit Chance": 15
+                    },
+                    "4": {
+                        "Weapon Name": "Disintegrator",
+                        "Weapon Damage": 25,
+                        "Weapon Accuracy": 95,
+                        "Weapon Crit Chance": 15
+                    }
+                }
+            },
+            "3": {
+                "Subclass Name": "Tactician",
+                "Subclass Abilities": {
+                    "3": "Order",
+                    "4": "Scan",
+                    "5": "Inspire",
+                    "6": "Plant Banner",
+                    "Passive 2": "Terrain Mapping"
+                },
+                "Subclass Weapon": {
+                    "3": {
+                        "Weapon Name": "Tesla Carbine",
+                        "Weapon Damage": 20,
+                        "Weapon Accuracy": 90,
+                        "Weapon Crit Chance": 15
+                    },
+                    "4": {
+                        "Weapon Name": "Disintegrator",
+                        "Weapon Damage": 25,
+                        "Weapon Accuracy": 95,
+                        "Weapon Crit Chance": 15
+                    }
+                }
+            }
+        }
+    },
+    "2": {
+        "Name": "Nanotech Sorcerer",
+        "Weapon": {
+            "1": {
+                "Weapon Name": "Old Pistol",
+                "Weapon Damage": 7,
+                "Weapon Accuracy": 90,
+                "Weapon Crit Chance": 15
+            },
+            "2": {
+                "Weapon Name": "Mag Revolver",
+                "Weapon Damage": 10,
+                "Weapon Accuracy": 93,
+                "Weapon Crit Chance": 16
+            }
+        },
+        "Armor": {
+            "1": {
+                "Armor Name": "Nanosuit",
+                "Armor Defense": 2,
+                "Armor Dodge Chance": 5
+            },
+            "2": {
+                "Armor Name": "Integrated Nano Systems Suit",
+                "Armor Defense": 4,
+                "Armor Dodge Chance": 7
+            },
+            "3": {
+                "Armor Name": "Helix Suit",
+                "Armor Defense": 6,
+                "Armor Dodge Chance": 9
+            }
+        },
+        "Class Abilities": {
+            "1": "Swarm",
+            "2": "Barrier",
+            "Passive 1": "Nanomachines",
+        },
+        "Subclass": {
+            "1": {
+                "Subclass Name": "Pyromancer",
+                "Subclass Abilities": {
+                    "3": "Scorch",
+                    "4": "Combust",
+                    "5": "Desiccate",
+                    "6": "Immolate",
+                    "Passive 2": "Hellfire"
+                },
+                "Subclass Weapon": {
+                    "3": {
+                        "Weapon Name": "Beam Pistol",
+                        "Weapon Damage": 15,
+                        "Weapon Accuracy": 96,
+                        "Weapon Crit Chance": 17
+                    },
+                    "4": {
+                        "Weapon Name": "Plasma Phaser",
+                        "Weapon Damage": 20,
+                        "Weapon Accuracy": 99,
+                        "Weapon Crit Chance": 18
+                    }
+                }
+            },
+            "2": {
+                "Subclass Name": "Cryomancer",
+                "Subclass Abilities": {
+                    "3": "Frostbite",
+                    "4": "Freeze",
+                    "5": "Avalanche",
+                    "6": "Glaciate",
+                    "Passive 2": "Coolant"
+                },
+                "Subclass Weapon": {
+                    "3": {
+                        "Weapon Name": "Beam Pistol",
+                        "Weapon Damage": 15,
+                        "Weapon Accuracy": 96,
+                        "Weapon Crit Chance": 17
+                    },
+                    "4": {
+                        "Weapon Name": "Plasma Phaser",
+                        "Weapon Damage": 20,
+                        "Weapon Accuracy": 99,
+                        "Weapon Crit Chance": 18
+                    }
+                }
+            },
+            "3": {
+                "Subclass Name": "Electromancer",
+                "Subclass Abilities": {
+                    "3": "Overcharge",
+                    "4": "Shock",
+                    "5": "Chain Lightning",
+                    "6": "Electrical Storm",
+                    "Passive 2": "Static"
+                },
+                "Subclass Weapon": {
+                    "3": {
+                        "Weapon Name": "Beam Pistol",
+                        "Weapon Damage": 15,
+                        "Weapon Accuracy": 96,
+                        "Weapon Crit Chance": 17
+                    },
+                    "4": {
+                        "Weapon Name": "Plasma Phaser",
+                        "Weapon Damage": 20,
+                        "Weapon Accuracy": 99,
+                        "Weapon Crit Chance": 18
+                    }
+                }
+            }
+        }
+    },
+    "3": {
+        "Name": "Assault Specialist",
+        "Weapon": {
+            "1": {
+                "Weapon Name": "Old Assault Rifle",
+                "Weapon Damage": 15,
+                "Weapon Accuracy": 80,
+                "Weapon Crit Chance": 10
+            },
+            "2": {
+                "Weapon Name": "Plasma Rifle",
+                "Weapon Damage": 20,
+                "Weapon Accuracy": 82,
+                "Weapon Crit Chance": 11
+            },
+        },
+        "Armor": {
+            "1": {
+                "Armor Name": "Infantry Armor",
+                "Armor Defense": 7,
+                "Armor Dodge Chance": 0
+            },
+            "2": {
+                "Armor Name": "Heavy Plate",
+                "Armor Defense": 10,
+                "Armor Dodge Chance": 0
+            },
+            "3": {
+                "Armor Name": "Exoskeleton",
+                "Armor Defense": 15,
+                "Armor Dodge Chance": 0
+            }
+        },
+        "Class Abilities": {
+            "1": "Fragmentation Grenade",
+            "2": "Dive Roll",
+            "Passive 1": "Fast Reactions",
+        },
+        "Subclass": {
+            "1": {
+                "Subclass Name": "Grenadier",
+                "Subclass Abilities": {
+                    "3": "Flashbang",
+                    "4": "Incendiary",
+                    "5": "Plasma Grenade",
+                    "6": "Volley",
+                    "Passive 2": ""
+                },
+                "Subclass Weapon": {
+                    "3": {
+                        "Weapon Name": "Laser Machinegun",
+                        "Weapon Damage": 25,
+                        "Weapon Accuracy": 84,
+                        "Weapon Crit Chance": 12
+                    },
+                    "4": {
+                        "Weapon Name": "Ion Autocannon",
+                        "Weapon Damage": 30,
+                        "Weapon Accuracy": 86,
+                        "Weapon Crit Chance": 13
+                    }
+                }
+            },
+            "2": {
+                "Subclass Name": "Technician",
+                "Subclass Abilities": {
+                    "3": "EMP Grenade",
+                    "4": "Gas Spray",
+                    "5": "Flamethrower",
+                    "6": "Detonate",
+                    "Passive 2": "Overdrive"
+                },
+                "Subclass Weapon": {
+                    "3": {
+                        "Weapon Name": "Laser Machinegun",
+                        "Weapon Damage": 25,
+                        "Weapon Accuracy": 84,
+                        "Weapon Crit Chance": 12
+                    },
+                    "4": {
+                        "Weapon Name": "Ion Autocannon",
+                        "Weapon Damage": 30,
+                        "Weapon Accuracy": 86,
+                        "Weapon Crit Chance": 13
+                    }
+                }
+            },
+            "3": {
+                "Subclass Name": "Vanguard",
+                "Subclass Abilities": {
+                    "3": "AP Shell",
+                    "4": "Scattershot",
+                    "5": "Mortar",
+                    "6": "Bulldoze",
+                    "Passive 2": "Bulwark"
+                },
+                "Subclass Weapon": {
+                    "3": {
+                        "Weapon Name": "Laser Machinegun",
+                        "Weapon Damage": 25,
+                        "Weapon Accuracy": 84,
+                        "Weapon Crit Chance": 12
+                    },
+                    "4": {
+                        "Weapon Name": "Ion Autocannon",
+                        "Weapon Damage": 30,
+                        "Weapon Accuracy": 86,
+                        "Weapon Crit Chance": 13
+                    }
+                }
+            }
+        }
+    },
+    "4": {
+        "Name": "Headhunter Assassin",
+        "Weapon": {
+            "1": {
+                "Weapon Name": "Old Submachine Gun",
+                "Weapon Damage": 15,
+                "Weapon Accuracy": 70,
+                "Weapon Crit Chance": 15
+            },
+            "2": {
+                "Weapon Name": "Laser Submachine Gun",
+                "Weapon Damage": 18,
+                "Weapon Accuracy": 74,
+                "Weapon Crit Chance": 17
+            }
+        },
+        "Armor": {
+            "1": {
+                "Armor Name": "Stealth Armor",
+                "Armor Defense": 2,
+                "Armor Dodge Chance": 10
+            },
+            "2": {
+                "Armor Name": "Distortion Suit",
+                "Armor Defense": 4,
+                "Armor Dodge Chance": 15
+            },
+            "3": {
+                "Armor Name": "Shadowweave Shroud",
+                "Armor Defense": 5,
+                "Armor Dodge Chance": 20
+            }
+        },
+        "Class Abilities": {
+            "1": "Stab",
+            "2": "Evade",
+            "Passive 1": "Stealth",
+        },
+        "Subclass": {
+            "1": {
+                "Subclass Name": "Gunslinger",
+                "Subclass Abilities": {
+                    "3": "Hollow Point Rounds",
+                    "4": "Quickdraw",
+                    "5": "From the Shadows",
+                    "6": "Unload",
+                    "Passive 2": "Precision"
+                },
+                "Subclass Weapon": {
+                    "3": {
+                        "Weapon Name": "Dual Mag Revolvers",
+                        "Weapon Damage": 20,
+                        "Weapon Accuracy": 85,
+                        "Weapon Crit Chance": 18
+                    },
+                    "4": {
+                        "Weapon Name": "Dual Beam Pistols",
+                        "Weapon Damage": 24,
+                        "Weapon Accuracy": 90,
+                        "Weapon Crit Chance": 19
+                    }
+                }
+            },
+            "2": {
+                "Subclass Name": "Sharpshooter",
+                "Subclass Abilities": {
+                    "3": "Headshot",
+                    "4": "Silence",
+                    "5": "Piercing Shot",
+                    "6": "",
+                    "Passive 2": "Scope"
+                },
+                "Subclass Weapon": {
+                    "3": {
+                        "Weapon Name": "Gauss Rifle",
+                        "Weapon Damage": 25,
+                        "Weapon Accuracy": 80,
+                        "Weapon Crit Chance": 18
+                    },
+                    "4": {
+                        "Weapon Name": "Railgun",
+                        "Weapon Damage": 30,
+                        "Weapon Accuracy": 84,
+                        "Weapon Crit Chance": 19
+                    }
+                }
+            },
+            "3": {
+                "Subclass Name": "Rogue",
+                "Subclass Abilities": {
+                    "3": "Slash",
+                    "4": "Shroud",
+                    "5": "Cripple",
+                    "6": "Hologram",
+                    "Passive 2": "Distraction"
+                },
+                "Subclass Weapon": {
+                    "3": {
+                        "Weapon Name": "Scattergun",
+                        "Weapon Damage": 20,
+                        "Weapon Accuracy": 80,
+                        "Weapon Crit Chance": 25
+                    },
+                    "4": {
+                        "Weapon Name": "Scattercannon",
+                        "Weapon Damage": 24,
+                        "Weapon Accuracy": 84,
+                        "Weapon Crit Chance": 33
+                    }
+                }
+            }
+        }
+    }
+}
